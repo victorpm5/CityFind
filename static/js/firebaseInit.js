788d92b4823e5633b6955004721939603b9cb319
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-04-22 13:46:14 +0200

    initialize first firebase files

---
+++ static/js/firebaseInit.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+
+    firebaseDatabase.init();
+
+}
+
+var constants = {
+    var gameReference = "game/";
+    var isPlayingReference = "/starCount";
+    var value = "value";
+}
+
+var firebaseDatabase = {
+
+    init: function() {
+        var config = {
+            apiKey: "AIzaSyAhoYEMnBrO-nNB_50LimITrkWHvwAcipM",
+            authDomain: "copenhacks2017.firebaseapp.com",
+            databaseURL: "https://copenhacks2017.firebaseio.com",
+            projectId: "copenhacks2017",
+            storageBucket: "copenhacks2017.appspot.com",
+            messagingSenderId: "1097292714945"
+        };
+        firebase.initializeApp(config);
+    }
+
+}
+
+var gameDatabase = {
+
+    writeGame : function(gameId, city) {
+        firebase.database().ref(constants.gameReference + userId).set({
+            city: city,
+            isPlaying: false
+        });
+    }
+
+    updatePlayingGame : function(gameId, isPlaying) {
+        var updateGame = {
+            isPlaying = isPlaying
+        }
+        var updates = {};
+        updates[constants.gameReference + gameId] = updateGame;
+        return firebase.database().ref().update(updates);
+    }
+
+    notifyWhenPlayingChanges : function() {
+        var gamePlayingRef = firebase.
+                database().ref( constants.gameReference +
+                                postId +
+                                constants.gamePlayingRef);
+        gamePlayingRef.on(constants.value, function(snapshot) {
+            // TODO notify users
+        });
+    }
+
+    getCityGame : function(gameId) {
+        return firebase.database().ref(constants.gameReference + gameId).once(constants.value)
+                .then(function(snapshot) {
+                  return snapshot.val().city;
+                });
+    }
+
+}
