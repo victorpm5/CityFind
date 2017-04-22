8bd639e70e9032f2441d249ede8c208f69285cf1
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-04-22 20:09:57 +0200

    create link to next templates

--- Main.py
+++ Main.py
@@ -1,12 +1,24 @@
-import Config, ComputerVision,Twilio
-from flask import Flask,  render_template
+import Config, ComputerVision, Twilio
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/create-game')
+def create_game():
+    return render_template('createGame.html')
+
+
+@app.route('/join-game')
+def join_game():
+    return render_template('joinGame.html')
+
 
 if __name__ == '__main__':
     app.run()
